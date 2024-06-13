af2bdaff6e55e871676ae7ca5ce32f1129767786
Chiril <BokKi48@yandex.ru>
2024-06-13 10:37:38 +0300

    KS-2-1 average mean

--- src/main/kotlin/lesson_1/lesson1_task4.kt
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -2,7 +2,7 @@ package org.example.lesson_1
 
 fun main() {
     val length: Long = 40868600000
-    val age: Short = 27
+    val age: Byte = 27
     val partOfDay: Float = 0.075F
     val seconds: Short = 6480
     val partOfYear: Double = 2.0547945205479453E-4
