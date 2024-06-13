89097559c4d6af317e13b2689029d35df2d309fc
Chiril <BokKi48@yandex.ru>
2024-06-13 23:34:16 +0300

    KS-2-3 refactor smth

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,18 +1,18 @@
 package org.example.lesson_2
 
-const val minutesInHour = 60
+const val MINUTES_IN_HOUR = 60
 
 fun main() {
     val departureHours = 9
     val departureMinutes = 39
-    val timeOfDepartureInMinutes = departureHours * minutesInHour + departureMinutes
+    val timeOfDepartureInMinutes = departureHours * MINUTES_IN_HOUR + departureMinutes
 
     val minutesInRoad = 457
     val timeOfArrivalInMinutes = timeOfDepartureInMinutes + minutesInRoad
 
 
-    val arrivalHours = timeOfArrivalInMinutes / minutesInHour
-    val arrivalMinutes = timeOfArrivalInMinutes % minutesInHour
+    val arrivalHours = timeOfArrivalInMinutes / MINUTES_IN_HOUR
+    val arrivalMinutes = timeOfArrivalInMinutes % MINUTES_IN_HOUR
 
     println("Время прибытия поезда - ${"%02d:%02d".format(arrivalHours, arrivalMinutes)}")
 
