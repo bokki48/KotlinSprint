b5c4def56d264dde4d805b35b412cbf650625cc9
Chiril <BokKi48@yandex.ru>
2024-06-13 20:05:31 +0300

    KS-3-1 greating user

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_3
+
+fun main() {
+    val userName = "Кирилл"
+    var userGreating = "Добрый день"
+    println("$userGreating, $userName!")
+
+    userGreating = "Добрый вечер"
+    println("$userGreating, $userName!")
+
+
+
+}
