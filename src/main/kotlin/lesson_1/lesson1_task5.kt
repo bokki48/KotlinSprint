3c04a586feee9806d221ca37d800f0ce80df769a
Chiril <BokKi48@yandex.ru>
2024-06-14 08:49:17 +0300

    KS-1-5 create const

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,13 +1,13 @@
 package org.example.lesson_1
 
-const val secondsInHour = 3600
-const val secondsInMinute = 60
+const val SECONDS_IN_HOUR = 3600
+const val SECONDS_IN_MINUTE = 60
 
 fun main() {
     val totalSecondsInSpace = 6429
-    val hoursInSpace = totalSecondsInSpace / secondsInHour
-    val minutesInSpace = (totalSecondsInSpace - hoursInSpace * secondsInHour) / secondsInMinute
-    val secondsInSpace = totalSecondsInSpace - hoursInSpace * secondsInHour - minutesInSpace * secondsInMinute
+    val hoursInSpace = totalSecondsInSpace / SECONDS_IN_HOUR
+    val minutesInSpace = (totalSecondsInSpace - hoursInSpace * SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
+    val secondsInSpace = totalSecondsInSpace - hoursInSpace * SECONDS_IN_HOUR - minutesInSpace * SECONDS_IN_MINUTE
 
-    println("%02d:%02d:%02d".format(hoursInSpace, minutesInSpace, secondsInSpace))
+    println("Гагарин провёл в космосе - ${"%02d:%02d:%02d".format(hoursInSpace, minutesInSpace, secondsInSpace)}")
 }
