596cdb8c2db3237286c8ef3617153b73e81a9a94
Chiril <BokKi48@yandex.ru>
2024-06-15 09:52:08 +0300

    KS-4-1 anyone refactor smth

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -4,7 +4,8 @@ const val TOTAL_COUNT_OF_TABLES = 13
 
 fun main() {
     val todayReserved = 13
-    val tomorrowReserved = 9
+    val tomorrowFree = 4
+    val tomorrowReserved = TOTAL_COUNT_OF_TABLES - tomorrowFree
 
     println("Доступность столиков на сегодня: ${todayReserved < TOTAL_COUNT_OF_TABLES}")
     println("Доступность столиков на завтра: ${tomorrowReserved < TOTAL_COUNT_OF_TABLES}")
