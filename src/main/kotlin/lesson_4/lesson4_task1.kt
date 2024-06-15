98cb2a65ced4461e7a483f5fef78e7071c1e368a
Chiril <BokKi48@yandex.ru>
2024-06-15 09:27:52 +0300

    KS-4-1 refactor const

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -1,10 +1,11 @@
 package org.example.lesson_4
 
+const val TOTAL_COUNT_OF_TABLES = 13
+
 fun main() {
-    val totalCountOfTables = 13
     val todayReserved = 13
     val tomorrowReserved = 9
 
-    println("Доступность столиков на сегодня: ${todayReserved < totalCountOfTables}")
-    println("Доступность столиков на завтра: ${tomorrowReserved < totalCountOfTables}")
+    println("Доступность столиков на сегодня: ${todayReserved < TOTAL_COUNT_OF_TABLES}")
+    println("Доступность столиков на завтра: ${tomorrowReserved < TOTAL_COUNT_OF_TABLES}")
 }
