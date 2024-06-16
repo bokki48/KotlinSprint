47e35806a281acd2a52ba6c54044a700d30e8158
Chiril <BokKi48@yandex.ru>
2024-06-16 14:11:00 +0300

    KS-5-5 refactor const

--- src/main/kotlin/lesson_5/lesson5_task5.kt
+++ src/main/kotlin/lesson_5/lesson5_task5.kt
@@ -2,12 +2,24 @@ package org.example.lesson_5
 
 import kotlin.random.Random
 
+const val THREE_MATCHED_NUMBERS = 3
+const val TWO_MATCHED_NUMBERS = 2
+const val ONE_MATCHED_NUMBERS = 1
+const val LOW_MATCHED_NUMBERS = 0
+const val NOT_INCLUDED_HIGH_MATCHED_NUMBERS = 43
 
 fun main() {
-    val firstWinningNumber = Random.nextInt(0, 42) % 43
-    val secondWinningNumber = Random.nextInt(0, 42) % 43
-    val thirdWinningNumber = Random.nextInt(0, 42) % 43
+    val firstWinningNumber =
+        Random.nextInt(LOW_MATCHED_NUMBERS, NOT_INCLUDED_HIGH_MATCHED_NUMBERS) % NOT_INCLUDED_HIGH_MATCHED_NUMBERS
+    val secondWinningNumber =
+        Random.nextInt(LOW_MATCHED_NUMBERS, NOT_INCLUDED_HIGH_MATCHED_NUMBERS) % NOT_INCLUDED_HIGH_MATCHED_NUMBERS
+    val thirdWinningNumber =
+        Random.nextInt(LOW_MATCHED_NUMBERS, NOT_INCLUDED_HIGH_MATCHED_NUMBERS) % NOT_INCLUDED_HIGH_MATCHED_NUMBERS
     val listWinningNumbers = listOf(firstWinningNumber, secondWinningNumber, thirdWinningNumber)
+
+    println(firstWinningNumber)
+    println(secondWinningNumber)
+    println(thirdWinningNumber)
 
     print("Введите первое число: ")
     val firstEnteringNumber = readLine()!!.toInt()
@@ -19,9 +31,9 @@ fun main() {
 
     val countOfMatchedNumbers = (listWinningNumbers.intersect(listEnteringNumbers)).size
     when {
-        countOfMatchedNumbers == 3 -> println("Поздравляем, Вы выиграли джекпот!")
-        countOfMatchedNumbers == 2 -> println("Вы выиграли крупный приз!")
-        countOfMatchedNumbers == 1 -> println("Не расстраиваетесь, Вы выиграли утешительный приз!")
+        countOfMatchedNumbers == THREE_MATCHED_NUMBERS -> println("Поздравляем, Вы выиграли джекпот!")
+        countOfMatchedNumbers == TWO_MATCHED_NUMBERS -> println("Вы выиграли крупный приз!")
+        countOfMatchedNumbers == ONE_MATCHED_NUMBERS -> println("Не расстраиваетесь, Вы выиграли утешительный приз!")
         else -> println("К сожалению, Вы не угадали ни одного числа.")
     }
 
