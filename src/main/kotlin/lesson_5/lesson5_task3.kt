e9cd6e8f1600e84835becfc47c5b1a3832aa7095
Chiril <BokKi48@yandex.ru>
2024-06-16 13:53:57 +0300

    KS-5-3 refactor const

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -1,5 +1,8 @@
 package org.example.lesson_5
 
+const val FIRST_WINNIG_NUMBER = 40
+const val SECOND_WINNIG_NUMBER = 25
+
 fun main() {
     print("Введите первое число от 0 до 42: ")
     val firstEnteringNumber = readLine()!!.toInt()
@@ -7,14 +10,13 @@ fun main() {
     print("Введите второе число от 0 до 42: ")
     val secondEnteringNumber = readLine()!!.toInt()
 
-    val firstWinningNumber = 40
-    val secondWinningNumber = 25
-
-    if ((firstEnteringNumber == 40 && secondEnteringNumber == 25) || (firstEnteringNumber == 25 && secondEnteringNumber == 40))
-        println("Поздравляем! Вы выиграли главный приз!")
-    else if (firstEnteringNumber == 40 || secondEnteringNumber == 25 || firstEnteringNumber == 25 || secondEnteringNumber == 40)
-        println("Вы выиграли утешительный приз!")
+    if ((firstEnteringNumber == FIRST_WINNIG_NUMBER && secondEnteringNumber == SECOND_WINNIG_NUMBER) ||
+        (firstEnteringNumber == SECOND_WINNIG_NUMBER && secondEnteringNumber == FIRST_WINNIG_NUMBER)
+    ) println("Поздравляем! Вы выиграли главный приз!")
+    else if (firstEnteringNumber == FIRST_WINNIG_NUMBER || secondEnteringNumber == SECOND_WINNIG_NUMBER ||
+        firstEnteringNumber == SECOND_WINNIG_NUMBER || secondEnteringNumber == FIRST_WINNIG_NUMBER
+    ) println("Вы выиграли утешительный приз!")
     else println("Неудача!")
 
-    println("Выигрышными числами являются: $firstWinningNumber и $secondWinningNumber")
+    println("Выигрышными числами являются: $FIRST_WINNIG_NUMBER и $SECOND_WINNIG_NUMBER")
 }
