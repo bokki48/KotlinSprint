5280e27c618dc6c33f3ccc68364fd4f1263a6098
Chiril <BokKi48@yandex.ru>
2024-06-16 13:28:34 +0300

    KS-4-5 a lot of refactor

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -1,24 +1,29 @@
 package org.example.lesson_4
 
+const val LOW_COUNT_OF_CREW = 55
+const val HIGH_COUNT_OF_CREW = 70
+const val LOW_COUNT_OF_BOXES = 50
+
 fun main() {
     print("Есть ли повреждения в корбале? ")
-    val bodyDamage = readLine()?.toBoolean()!!
+    val isBodyDamage = readln().toBoolean()
 
     print("Сколько человек на борту? ")
-    val countOfCrew = readLine()?.toInt()!!
+    val countOfCrew = readln().toInt()
 
     print("Сколько ящиков провизии? ")
-    val countOfBoxes = readLine()?.toInt()!!
+    val countOfBoxes = readln().toInt()
 
     print("Погодные условия благоприятны? ")
-    val favorableWeatherConditions = readLine()?.toBoolean()!!
+    val isWeatherConditionsFavorable = readln().toBoolean()
 
-    val recommendedCountOfCrew = countOfCrew in 55..70
-    val recommendedCountOfBoxes = countOfBoxes > 50
+    val isRecommendedCountOfCrew = countOfCrew in LOW_COUNT_OF_CREW..HIGH_COUNT_OF_CREW
+    val isRecommendedCountOfBoxes = countOfBoxes > LOW_COUNT_OF_BOXES
 
-    val alternativeCountOfCrew = countOfCrew == 70
-    val alternativeCountOfBoxes = countOfBoxes >= 50
+    val isAlternativeCountOfCrew = countOfCrew == HIGH_COUNT_OF_CREW
+    val isAlternativeCountOfBoxes = countOfBoxes >= LOW_COUNT_OF_BOXES
 
-    val setSail = (!bodyDamage && recommendedCountOfCrew && recommendedCountOfBoxes) || (bodyDamage && alternativeCountOfCrew && alternativeCountOfBoxes && favorableWeatherConditions)
+    val setSail = (!isBodyDamage && isRecommendedCountOfCrew && isRecommendedCountOfBoxes)
+            || (isBodyDamage && isAlternativeCountOfCrew && isAlternativeCountOfBoxes && isWeatherConditionsFavorable)
     println("Благоприятные ли условия для отправки корбаля в плавание? $setSail")
 }
