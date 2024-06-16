243fbb877eb6a0e4c4e597d9b431d5554a594652
Chiril <BokKi48@yandex.ru>
2024-06-16 12:30:55 +0300

    KS-4-4 refactor is even

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -1,11 +1,13 @@
 package org.example.lesson_4
 
+const val DAY = 5
+
 fun main() {
-    val day = 5
-    val exersisesForHands = day % 2 != 0
-    val exersisesForPress = day % 2 != 0
-    val exersisesForLegs = day % 2 == 0
-    val exersisesForBack = day % 2 == 0
+    val isEven = DAY % 2 == 0
+    val exersisesForHands = !isEven
+    val exersisesForPress = !isEven
+    val exersisesForLegs = isEven
+    val exersisesForBack = isEven
 
     println(
         "Упражнения для рук:   $exersisesForHands\nУпражнения для ног:   $exersisesForLegs\n" +
