5daa248a2cdd8ad1675125e5885a60669d47d104
Chiril <BokKi48@yandex.ru>
2024-06-16 12:37:11 +0300

    KS-3-5 refactor split

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -2,9 +2,11 @@ package org.example.lesson_3
 
 fun main() {
     val chessMoveData = "D2-D4;0"
-    val cageFrom = chessMoveData.substring(0,2)
-    val cageWhere = chessMoveData.substring(3,5)
-    val strokeNumber = chessMoveData.substring(6)
+    val parsChessMoveData = chessMoveData.split('-', ';')
+
+    val cageFrom = parsChessMoveData.get(0)
+    val cageWhere = parsChessMoveData.get(1)
+    val strokeNumber = parsChessMoveData.get(2)
 
     println("Фигура пошла с клетки: $cageFrom")
     println("Фигура была поставлена на клетку: $cageWhere")
