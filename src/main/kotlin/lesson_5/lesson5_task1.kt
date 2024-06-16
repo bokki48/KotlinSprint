3b2ff8cfbbcd4eb8239ceb5587c0be08eaf1e9d0
Chiril <BokKi48@yandex.ru>
2024-06-16 13:47:21 +0300

    KS-5-1 refactor const

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -1,9 +1,11 @@
 package org.example.lesson_5
 
+const val CORRECT_ANSWCER = 94
+
 fun main() {
     print("Решите пример: 45 + 49 = ")
     val userAnswer = readLine()?.toInt()
 
-    if (userAnswer == 94) println("Добро пожаловать!")
+    if (userAnswer == CORRECT_ANSWCER) println("Добро пожаловать!")
     else println("Доступ запрещён.")
 }
