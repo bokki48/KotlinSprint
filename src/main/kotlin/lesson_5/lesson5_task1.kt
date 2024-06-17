719c863bedb1da4079903030e85271617f0b4a25
Chiril <BokKi48@yandex.ru>
2024-06-17 12:39:33 +0300

    KS-5-1 refactor summand

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -1,11 +1,13 @@
 package org.example.lesson_5
 
-const val CORRECT_ANSWCER = 94
+const val CORRECT_ANSWER = 94
+const val FIRST_SUMMAND = 45
+const val SECOND_SUMMAND = 49
 
 fun main() {
-    print("Решите пример: 45 + 49 = ")
+    print("Решите пример: $FIRST_SUMMAND + $SECOND_SUMMAND = ")
     val userAnswer = readln().toInt()
 
-    if (userAnswer == CORRECT_ANSWCER) println("Добро пожаловать!")
+    if (userAnswer == CORRECT_ANSWER) println("Добро пожаловать!")
     else println("Доступ запрещён.")
 }
